528512b7c94a514bd07f8bc88e69e66b75f5c633
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-07-03 12:01:06 +0700

    feat: created nested loop

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,23 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+/*
+ *ให้สร้าง Function printStar Function นี้จะรับตัวเลขเข้ามา 1 ตัว แล้วแสดงผลลัพธ์ตามตัวเลขที่ได้รับมา เช่น ถ้าตัวเลขที่รับมาคือ 5 จะได้ผลลัพธ์ดังนี้
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+ */
+function printStar(n) {
+  let finalResult = "";
+  for (let i = 1; i <= n; i++) {
+    let rowResult = "";
+    for (let j = 0; j < i; j++) {
+      rowResult += "* ";
+    }
+    console.log(`รอบที่ ${i} ได้ ${rowResult}`);
+    finalResult += rowResult + "\n";
+  }
+  return finalResult;
 }
+
+console.log(printStar(5));
